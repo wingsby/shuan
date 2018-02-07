2cd57a58236d717fb83a438c07ae3ea027444a4a
wingsby <wingsby@163.com>
2018-02-08 00:06:14 +0800

    semifinal v0.5

---
+++ old/test2.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+# @Author: wangye
+# @Description:
+# @Date:Created  on 20:28 2018/2/7.
+# @Modify
+#======================shaun=======================================
+import copy
+# array=dict()
+# array[1]=[2,3]
+# array[2]=[4,5]
+#
+# def fun(array):
+#     # a=copy.copy(array[1])
+#     a=array[1]
+#     a.append(4)
+#     print(len(array[1]))
+#
+#
+# fun(array)
+
+a=dict()
+a[0]=(3,'10')
+a[1]=(7,"111")
+if 3 in a.values()[:,0]:
+    print(3)
